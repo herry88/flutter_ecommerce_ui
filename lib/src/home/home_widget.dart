56d4d493ceda678db403fdc3d24ea0fd2994bd5e
herry88 <herry081288@gmail.com>
2021-07-08 02:18:10 +0700

    RefreshIndicaorHomeWidget

--- lib/src/home/home_widget.dart
+++ lib/src/home/home_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutteruinew/config/icons.dart';
 import 'package:flutteruinew/src/widget/flashsale.dart';
 import 'package:flutteruinew/model/product.dart';
 import 'package:flutteruinew/src/widget/flashsaleslider.dart';
@@ -14,20 +15,63 @@ class HomeWidget extends StatefulWidget {
 
 class _HomeWidgetState extends State<HomeWidget> {
   ProductsList _productList = ProductsList();
+  List<String>? _demoData;
+  @override
+  void initState() {
+    // initializing state / demo data
+    var productList = _productList;
+    _demoData = [
+      productList.toString(),
+    ];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20.0,
+    return RefreshIndicator(
+      onRefresh: () {
+        return Future.delayed(
+          Duration(
+            seconds: 1,
           ),
-          child: SearchWidget(),
-        ),
-        SliderWidget(),
-        FlashSaleWidget(),
-        FlashSaleSliderWidget(heroTag:'home_flash_sales', productsList: _productList.flashSalesList),
-      ],
+        );
+      },
+      child: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+            ),
+            child: SearchWidget(),
+          ),
+          SliderWidget(),
+          FlashSaleWidget(),
+          FlashSaleSliderWidget(
+            heroTag: 'home_flash_sales',
+            productsList: _productList.flashSalesList,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 8.0,
+            ),
+            child: ListTile(
+              dense: true,
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 0,
+              ),
+              leading: Icon(
+                UiIcons.favorites,
+                color: Theme.of(context).hintColor,
+              ),
+              title: Text(
+                'Recommended Product KW',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
