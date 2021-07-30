3e6fcd8b42e3074d9b88472af4bb1b6802ede550
herry88 <herry081288@gmail.com>
2021-07-30 09:29:18 +0700

    categories donw

--- lib/src/screen/categories/categories.dart
+++ lib/src/screen/categories/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutteruinew/src/widget/drawer.dart';
+import 'package:flutteruinew/src/widget/shoppingcart.dart';
 
 class CategoryWidget extends StatefulWidget {
   const CategoryWidget({Key? key}) : super(key: key);
@@ -8,18 +10,46 @@ class CategoryWidget extends StatefulWidget {
 }
 
 class _CategoryWidgetState extends State<CategoryWidget> {
+  // CategoriesList _categoriesList = new CategoriesList();
+  // SubCategoriesList _subCategoriesList = new SubCategoriesList();
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: ClipRRect(
-      borderRadius: BorderRadius.circular(
-        8.0,
+    return Scaffold(
+      drawer: DrawerWidget(),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: Icon(
+            Icons.sort,
+            color: Theme.of(context).hintColor,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          'Categories',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        actions: <Widget>[
+          new ShoppingCartButton(
+              iconColor: Theme.of(context).hintColor,
+              labelColor: Theme.of(context).accentColor),
+          Container(
+              width: 30,
+              height: 30,
+              margin: EdgeInsets.only(top: 12.5, bottom: 12.5, right: 20),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(300),
+                onTap: () {
+                  Navigator.of(context).pushNamed('/Tabs', arguments: 1);
+                },
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('img/user2.jpg'),
+                ),
+              )),
+        ],
       ),
-      child: Image.asset(
-        'img/under.png',
-        height: 150.0,
-        width: 100.0,
-      ),
-    ));
+    );
   }
 }
