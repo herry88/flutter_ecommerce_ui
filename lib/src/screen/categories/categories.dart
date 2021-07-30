a7acb65020de44554519faedb94e4ff26c1ef07e
herry88 <herry081288@gmail.com>
2021-07-30 10:19:11 +0700

    categoryDoneLagi

--- lib/src/screen/categories/categories.dart
+++ lib/src/screen/categories/categories.dart
@@ -212,6 +212,133 @@ class _CategoryWidgetState extends State<CategoryWidget> {
   }
 
   Widget buildOddCategory(BuildContext context, Category category) {
-    return Column();
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                    color: Theme.of(context).hintColor.withOpacity(0.10),
+                    offset: Offset(0, 4),
+                    blurRadius: 10)
+              ],
+            ),
+            constraints: BoxConstraints(minHeight: 120),
+            child: Wrap(
+              runAlignment: WrapAlignment.center,
+              spacing: 10,
+              runSpacing: 5,
+              children: List.generate(_subCategoriesList.list!.length, (index) {
+                SubCategory subCategory =
+                    _subCategoriesList.list!.elementAt(index);
+                return Material(
+                  borderRadius: BorderRadius.circular(30),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.of(context).pushNamed('/Category',
+                          arguments: RouteArgument(
+                              id: index, argumentsList: [category]));
+                    },
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(
+                            color:
+                                Theme.of(context).hintColor.withOpacity(0.2)),
+                      ),
+                      child: Text(
+                        subCategory.name,
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 120,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Theme.of(context).hintColor.withOpacity(0.10),
+                          offset: Offset(0, 4),
+                          blurRadius: 10)
+                    ],
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomLeft,
+                        end: Alignment.topRight,
+                        colors: [
+                          Theme.of(context).accentColor,
+                          Theme.of(context).accentColor.withOpacity(0.2),
+                        ])),
+                child: Column(
+                  children: <Widget>[
+                    Hero(
+                      tag: category.id,
+                      child: Icon(
+                        category.icon,
+                        color: Theme.of(context).primaryColor,
+                        size: 40,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      category.name,
+                      style: TextStyle(color: Theme.of(context).primaryColor),
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                right: -40,
+                bottom: -60,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor.withOpacity(0.08),
+                    borderRadius: BorderRadius.circular(150),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: -30,
+                top: -60,
+                child: Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor.withOpacity(0.12),
+                    borderRadius: BorderRadius.circular(150),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
